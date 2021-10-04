04f0ae444e729efc8b44f428ec7ab64f8f8d9be3
sadat.malik <sadat.malik@me.com>
2021-10-04 18:03:19 +0100

    initial files

---
+++ src/main/java/platform/codingnomads/co/springfundamentals/example/SpringFundamentalsExampleApplication.java
@@ -0,0 +1,13 @@
+package platform.codingnomads.co.springfundamentals.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFundamentalsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFundamentalsExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/platform/codingnomads/co/springfundamentals/example/SpringFundamentalsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package platform.codingnomads.co.springfundamentals.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFundamentalsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
